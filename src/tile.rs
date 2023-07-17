5077eea1287bae8bcf23bca5375a80a253174c26
secona <77039267+secona@users.noreply.github.com>
2023-07-17 16:08:34 +0700

    feat: implement tile

---
+++ src/tile.rs
@@ -0,0 +1,46 @@
+#[derive(PartialEq)]
+pub enum Value {
+    Number(i32),
+    Mine,
+}
+
+#[derive(PartialEq)]
+pub enum State {
+    Opened,
+    Closed,
+    Flagged,
+}
+
+pub struct Tile {
+    value: Value,
+    state: State,
+}
+
+impl Default for Tile {
+    fn default() -> Self {
+        Self {
+            value: Value::Number(0),
+            state: State::Closed,
+        }
+    }
+}
+
+impl Tile {
+    pub fn open(&mut self) -> Result<(), ()> {
+        if self.value == Value::Mine {
+            return Err(());
+        }
+
+        if self.state == State::Closed {
+            self.state = State::Opened;
+        }
+
+        Ok(())
+    }
+
+    pub fn flag(&mut self) {
+        if self.state == State::Closed {
+            self.state = State::Flagged;
+        }
+    }
+}
