bd5acc0f08a333d61922bd6d19132b9ed67d8342
secona <77039267+secona@users.noreply.github.com>
2023-07-17 18:08:59 +0700

    refactor: add tile_at method to Board

--- src/board.rs
+++ src/board.rs
@@ -9,28 +9,34 @@ pub struct Board {
 
 impl Board {
     pub fn new(width: usize, height: usize, mine_count: usize) -> Self {
-        let mut tiles = vec![vec![tile::Tile::default(); width]; height];
+        let tiles = vec![vec![tile::Tile::default(); width]; height];
+
+        let mut result = Self {
+            tiles,
+            height,
+            width,
+        };
 
         for _ in 0..mine_count {
             let random = Point::random(0..width, 0..height);
-            tiles[random.x][random.y].replace_value(tile::Value::Mine);
+            result.tile_at(&random).replace_value(tile::Value::Mine);
 
             for neighbor in random.neighbors() {
                 if neighbor.x >= width || neighbor.y >= height {
                     continue;
                 }
 
-                let tile = &mut tiles[neighbor.x][neighbor.y];
+                let tile = &mut result.tile_at(&neighbor);
                 if let tile::Value::Number(num) = tile.value() {
                     tile.replace_value(tile::Value::Number(num + 1))
                 }
             }
         }
 
-        Self {
-            tiles,
-            height,
-            width,
-        }
+        result
+    }
+
+    fn tile_at(&mut self, point: &Point) -> &mut tile::Tile {
+        &mut self.tiles[point.x][point.y]
     }
 }
